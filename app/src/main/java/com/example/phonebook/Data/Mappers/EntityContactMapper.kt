b7c40c229837d8e036fc050a7ef2b1875a41333c
Nikita Sheverdyaev <nikita.sheverdyaev@yandex.ru>
2022-08-14 14:07:50 +0300

    feat: Implement datas mapper

--- app/src/main/java/com/example/phonebook/Data/Mappers/EntityContactMapper.kt
+++ app/src/main/java/com/example/phonebook/Data/Mappers/EntityContactMapper.kt
@@ -1,13 +1,25 @@
 package com.example.phonebook.Data.Mappers
 
+import com.example.phonebook.Data.Entity.ContactTableModel
 import com.example.phonebook.Domain.Contact
+import com.example.phonebook.Domain.ContactInformation
 
 class EntityContactMapper {
-    fun toContactTableModel(contact: Contact) {
+    fun toContactTableModel(contact: Contact) : ContactTableModel {
         return ContactTableModel(
             name = contact.contactInf.name,
             phoneNumber = contact.contactInf.phoneNumber,
             contactId = contact.id,
+        )
+    }
+
+    fun toDomainContactModel(contact: ContactTableModel) : Contact {
+        return Contact(
+            id = contact.contactId!!,
+            ContactInformation(
+                name = contact.name,
+                phoneNumber = contact.phoneNumber,
+            )
         )
     }
 }
